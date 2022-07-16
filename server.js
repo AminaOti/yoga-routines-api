e9ceb29662c9a27941575ef9a5cbf3f4ab0950c2
Amina N Oti <Amina.Oti@ibm.com>
2022-07-16 16:33:38 +0100

    feat: project setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+//const router = require('./src/routes');
+//var cors = require('cors');
+
+
+const app = express();
+app.locals.title = 'Yoga Routines API'
+
+//app.use(cors());
+
+//app.use('/', router);
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT} for API: ${app.locals.title}`));
+
+
+// get a list of assetNames
+// get buySell data for each asset
